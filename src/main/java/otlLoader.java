58757b5c482e954c50f2482b1cefcb2936df1eb7
Geon Heo <reminiel@kaist.ac.kr>
2019-12-11 16:21:54 +0900

    Do not add lib files or iml, else setting files

--- src/main/java/otlLoader.java
+++ src/main/java/otlLoader.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 
 public class otlLoader {
-
     private final static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
 
     public otlLoader(){
